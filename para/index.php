333e969ea92a5ab62f022ef82875a499bd4797bb
sugiponn <43834820+sugiponn@users.noreply.github.com>
2018-10-04 11:10:33 +0900

    Add files via upload

---
+++ para/index.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>ホームページ制作、ビデオ編集、SEO対策、FLASH制作。TSインタラクティブ株式会社2015（東京都墨田区）</title>
+<meta name="description" content="TSインタラクティブ株式会社はビジネスブログ構築会社です。出張ホームページ更新予約受付中。MovableType、Flashや動画サイトを構築の提案をしています。" />
+<meta name="keywords" content="MovableType,Flash,ビジネスブログ,ブログ構築,出張ホームページ更新,TSinteractive,TSinteractive inc" />
+
+<script type="text/javascript">
+if ((navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('iPad') > 0 || navigator.userAgent.indexOf('Android') > 0) ) {
+    document.write('<link rel="stylesheet" type="text/css" href="style_i.css">');
+}else {
+        document.write('<link rel="stylesheet" type="text/css" href="style.css">');
+    }
+</script>
+
+<link href="slide.css" rel="stylesheet" type="text/css" />
+<link href="jquery.bxslider.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript" src="scripts/jquery.parallax-1.1.3.js"></script>
+<script type="text/javascript" src="scripts/jquery.localscroll-1.2.7-min.js"></script>
+<script type="text/javascript" src="scripts/jquery.scrollTo-1.4.2-min.js"></script>
+<script type="text/javascript" src="scripts/jquery.bxslider.min.js"></script>
+<script type="text/javascript" src="scripts/html5.js"></script>
+<script type="text/javascript" src="scripts/common.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+	$('#nav').localScroll(800);
+	
+	//.parallax(xPosition, speedFactor, outerHeight) options:
+	//xPosition - Horizontal position of the element
+	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+	$('#intro').parallax("50%", 0.1);
+	$('#second').parallax("50%", 0.1);
+	$('.bg').parallax("50%", 0.4);
+	$('#third').parallax("50%", 0.3);
+
+})
+
+</script>
+<!--[if lt IE 9]>
+      <script src="./js/html5.js"></script>
+<![endif]-->  
+
+
+</head>
+
+<body>
+	<ul id="nav">
+		<li><a href="#intro" title="Next Section"><img src="images/tsi_logo.gif" alt="Link" width="100" style="padding-top:5px;" /></a></li>
+	    <li><a href="#second" title="Next Section" class="navi_txt"><span class="txt_red">M</span>essage</a></li>
+	    <li><a href="#third" title="Next Section" class="navi_txt"><span class="txt_red">P</span>roject</a></li>
+	    <li><a href="#fifth" title="Next Section" class="navi_txt"><span class="txt_red">A</span>bout US</a></li>
+	</ul>
+	
+	<div id="intro">
+		<div class="story">
+	    	<div class="float-left">
+			<h2><span class="txt_red">H</span>ome</h2>
+	        <p>インターネットを通じて
+              <br>
+              物語（ストーリー）を<br>
+              作ることが <br>
+              僕たちの仕事です。<br> <br>
+<ul class="bxslider">
+    <li><img src="https://www.tsiii.com/para/images/slide/s01.png" alt="Slideshow Image 1" class="active" /></li>
+    <li><img src="https://www.tsiii.com/para/images/slide/s11.png" alt="Slideshow Image 11" /></li>
+    <li><img src="https://www.tsiii.com/para/images/slide/s12.png" alt="Slideshow Image 12" /></li>
+    <li><img src="https://www.tsiii.com/para/images/slide/s07.png" alt="Slideshow Image 7" /></li>
+    <li><img src="https://www.tsiii.com/para/images/slide/s13.png" alt="Slideshow Image 13" /></li>
+    <li><img src="https://www.tsiii.com/para/images/slide/s03.png" alt="Slideshow Image 2" /></li>
+    <li><img src="https://www.tsiii.com/para/images/slide/s04.png" alt="Slideshow Image 4" /></li>
+    <li><img src="https://www.tsiii.com/para/images/slide/s05.png" alt="Slideshow Image 5" /></li>
+    <li><img src="https://www.tsiii.com/para/images/slide/s06.png" alt="Slideshow Image 6" /></li>
+    <li><img src="https://www.tsiii.com/para/images/slide/s08.png" alt="Slideshow Image 8" /></li>
+    <li><img src="https://www.tsiii.com/para/images/slide/s09.png" alt="Slideshow Image 9" /></li>
+    <li><img src="https://www.tsiii.com/para/images/slide/s02.png" alt="Slideshow Image 3" /></li>
+</ul>
+     </p>
+     <!--p><a href="http://service.tsiii.com/" target="_blank"><span class="txt_red">TSIサービスはこちらです。</span></a></p-->
+     
+          </div>
+            
+            
+	    </div> <!--.story-->
+
+	</div> <!--#intro-->
+	<div class="bg"></div>
+	<div id="second">
+		<div class="story"><div class="bg"></div>
+	    	<div class="float-right">
+              <h2><span class="txt_red">M</span>essage</h2>
+	            <p>そう、<br>
+	              お客様の
+	              物語（ストーリー）を<br>
+	              作ることが、<br>
+              とってもとっても、<br>
+楽しみなのです。                
+	            <p>あなたはどんな物語を聞かせてくれますか？<br><br><br>
+              <video id="example_video_1" class="video-js vjs-default-skin" controls preload="none" width="500" height="280"
+      poster="https://www.tsiii.com/pixar/vod/library/media/01/penguinfan.png"
+      data-setup="{}">
+    <source src="https://www.tsiii.com/pixar/vod/library/media/01/penguinfan.mp4" type='video/mp4' />
+    <source src="https://www.tsiii.com/pixar/vod/library/media/01/penguinfan.webm" type='video/webm' />
+    <source src="https://www.tsiii.com/pixar/vod/library/media/01/penguinfan.ogv" type='video/ogg' />
+    <!--<track kind="captions" src="demo.captions.vtt" srclang="en" label="English"></track>--><!-- Tracks need an ending tag thanks to IE9 -->
+</video>
+</p>
+	        </div>
+	    </div> <!--.story-->
+	    
+	</div> <!--#second-->
+	
+	<div id="third">
+		<div class="story">
+	    	<div class="float-left">
+	        	<h2><span class="txt_red">P</span>roject</h2>
+	            <p>そして、<br>
+	              僕たちも<br>
+自分たちの<br>
+物語を作りたい。<br>
+</p>
+<ul>
+<li><a href="http://www.ecobike.jp/" target="_blank"><img src="images/ecbanner_w230.gif" width="230" height="99" alt="ecoBike.jp"/></a></li>
+<li><a href="http://preservedflower.hanadaigaku.com/" target="_blank"><img src="images/hanadaigakulogo.jpg" width="230" height="63" alt="花大学.com"/></a></li>
+</ul>
+	        </div>
+	    </div> <!--.story-->
+	</div> <!--#third-->
+	
+	<div id="fifth">
+		<div class="story">
+		  
+          <div style="float:right;width:540px;"><br><br>
+            <h3>会社概要</h3>
+            <table class="about">
+              <tbody><tr class="style_default">
+                <td width="100" valign="top" class="ttl">　社　名</td>
+                <td width="380" class="txt">ＴＳインタラクティブ株式会社<br>
+        (TSInteractive Inc.)</td>
+              </tr>
+              <tr class="style_default">
+                <td class="ttl">　代表取締役</td>
+                <td class="txt">友重　たくじ</td>
+              </tr>
+              <tr class="style_default">
+                <td class="ttl">　取締役</td>
+                <td align="left" class="txt">椙田　浩平</td>
+              </tr>
+              <tr class="style_default">
+                <td class="ttl">　設　立</td>
+                <td class="txt">平成16年7月1日<br></td>
+              </tr>
+              <tr class="style_default">
+                <td class="ttl">　資本金</td>
+                <td class="txt">3,000,000円</td>
+              </tr>
+              <tr class="style_default">
+                <td class="ttl">　住　所</td>
+                <td class="txt">〒130-0002<br>
+          東京都墨田区業平4-10-6和泉ビル102<br>
+         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12958.944110691276!2d139.814368!3d35.708113!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa2f59b7cd50ff797!2zVFPjgqTjg7Pjgr_jg6njgq_jg4bjgqPjg5bmoKrlvI_kvJrnpL4!5e0!3m2!1sja!2sjp!4v1438332431366" width="380" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
+         </td>
+              </tr>
+              <tr class="style_default">
+                <td class="ttl">　TEL</td>
+                <td class="txt">03-3829-5753</td>
+              </tr>
+              <tr class="style_default">
+                <td valign="top" class="ttl">　FAX</td>
+                <td class="txt">050-3730-8169<br></td>
+              </tr>
+              <tr class="style_default">
+                <td align="left" valign="top" class="ttl">　業務内容</td>
+                <td class="txt">・サイト構築（ホームページ制作、企画、保守、運用）<br>
+        ・グラフィックデザイン、オーサリング制作<br>
+        ・Webマーケティング、企画、コンサルティング<br>・プリザーブドフラワー関連商品通信販売<br>・スポーツ情報サイト運営<br></td>
+              </tr>
+          </tbody></table>
+          </div>
+<h2><span class="txt_red">A</span>bout Us</h2>
+<p>
+          <ul>
+              <li>通常サイトは<a href="https://www.tsiii.com/index.php">こちら</a><br>
+              お問い合わせは<a href="https://www.tsiii.com/query/" target="_blank">こちら</a></li>
+          </ul>
+</p>            
+          <h3>Link</h3>
+		<? include("../xml_rss/rsssumidablog.php"); ?>
+	    </div> <!--.story-->
+        <div class="copyright"><br>
+ <div align="center"><br>
+Copyright© <a href="./" title="ビジネスブログ構築、SEO対策、FLASH携帯動画配信、FLASHオーサリング制作のTSインタラクティブ(東京都墨田区)">TSインタラクティブ株式会社</a> TS Interactive inc., All rights reserved. </div>
+</div>
+	</div> <!--#fifth-->
+</body>
+</html>
